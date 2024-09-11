a59ae5681eed44fbd52c7fa314a0b30de648b6f8
ashoordoushow <ashoor12323@gmail.com>
2024-09-11 14:24:59 -0500

    products show action and test

--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -1,9 +1,10 @@
-json.id2332 product.id
-json.descriptionHELLO product.description
-json.image_url product.image_url
+json.id product.id          #I can change all the key labels (first part) in product jbuilder file ex. 'json.id1232' and it will ouput the key 'id1232' when ran through httpie
 json.name product.name
 json.price product.price
-
+json.image_url product.image_url
+json.description product.description
+json.created_at product.created_at
+json.updated_at product.updated_at
 
 
 
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,7 +15,24 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
         post "/products.json", params: { name: "lake", price: 500, image_url: "http:/jksdvknsdk", description: "nice" }
         assert_response 200
       end
-    end                                   
+    end      
+    
+    test "show" do
+      get "/products/#{Product.first.id}.json"
+      assert_response 200
+  
+      data = JSON.parse(response.body)
+      assert_equal ["id", "name", "price", "image_url", "description", "created_at", "updated_at"], data.keys
+    end
+
+    test "update" do
+      product = Product.first
+      patch "/products/#{product.id}.json", params: { name: "Updated name" }
+      assert_response 200
+  
+      data = JSON.parse(response.body)
+      assert_equal "Updated name", data["name"]
+    end
 end                                     
 
 
